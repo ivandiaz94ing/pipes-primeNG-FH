5bf14c24b7bdecb9311c06848c78e21b32fdfa14
Ivan Arturo <ivandiaz94ing@gmail.com>
2024-01-14 14:20:54 -0500

    pipes i18nSelect - i18nPlural

--- src/app/products/pages/uncommon-page/uncommon-page.component.ts
+++ src/app/products/pages/uncommon-page/uncommon-page.component.ts
@@ -7,4 +7,31 @@ import { Component } from '@angular/core';
 })
 export class UncommonPageComponent {
 
+  //i18nSelect Pipe
+  public name: string = ' Ivan ';
+  public gender : 'male' | 'female' = 'male';
+
+  public invitationMap = {
+    male: 'invitarlo',
+    female: 'invitarla',
+  }
+
+  cambiarPersona(){
+    this.name = 'Katty';
+    this.gender = 'female';
+  }
+
+  //i18nPlural Pipe
+  public clientes : string[] = ['Rafael', 'Marina','Monica','Rafita','Ivan','Yandy','Isabel','Sharoll','Sergio'];
+  public clientsMap = {
+    '=0' : 'no tememos ningun cliente esperando ',
+    '=1' : 'tenemos un cliente esperando',
+    '=2' : 'tenemos dos clientes esperando ',
+    'other' : 'tenemos # clientes esperando '
+  }
+
+  borrarCliente(): void{
+    this.clientes.shift();
+  }
+
 }
