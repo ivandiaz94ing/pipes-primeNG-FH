df8c48264108e83f2e7b03d9955e830a05e184fa
Ivan Arturo <ivandiaz94ing@gmail.com>
2024-01-13 20:05:22 -0500

    first commit de la section eleven of course

---
+++ src/app/prime-ng/prime-ng.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import {MenubarModule} from 'primeng/menubar';
+import { MenuModule } from 'primeng/menu';
+
+
+
+@NgModule({
+  exports:[
+    MenubarModule,
+    MenuModule,
+  ]
+
+})
+export class PrimeNgModule { }
